c3eccc583cab3b6195aac3168c56e4e67525af84
Siddharth <sidpatel789@gmail.com>
2017-11-19 03:38:05 -0500

    Board Static GUI

--- src/MoveBox.js
+++ src/MoveBox.js
@@ -4,10 +4,7 @@ export default class MoveBox extends Component{
 	render(){
 		console.log("Hi 2");
 		return(
-			<div>
-				<h1>Hello From Move</h1>
-				<h2>Box</h2>
-			</div>
+			<div></div>
 		)
 	}
 }
