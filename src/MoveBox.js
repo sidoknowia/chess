486e62906999d26ed6f358dc1e6615756a78520c
Siddharth <sidpatel789@gmail.com>
2017-12-02 13:41:07 -0500

    Grid CSS Updates

--- src/MoveBox.js
+++ src/MoveBox.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Board from './Board';
+import Game from './Game';
 
 export default class MoveBox extends Component{
+	constructor(){
+		super();
+		let chess_gm = new Game();
+	}
+
 	render(){
-		console.log("Hi 2");
 		return(
-			<div></div>
+			<div className="move-box">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+			</div>
 		)
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GameLoader from './GameLoader';
-//import MoveBox from './GameLoader';
+//import MoveBox from './MoveBox';
 import Board from './Board';
 import Game from './Game';
 
 //const Board = require("./Board.js");
 //const Game = require('./Game.js');
 
-let chess_gm = new Game();
+// let chess_gm = new Game();
 
-console.log(chess_gm);
+// console.log(chess_gm);
 
-chess_gm.make_move('W', 'P', 'e2', 'e4');
-chess_gm.make_move('B', 'P', 'e7', 'e5');
+// chess_gm.make_move('W', 'P', 'e2', 'e4');
+// chess_gm.make_move('B', 'P', 'e7', 'e5');
 
-chess_gm.make_move('W', 'N', 'g1', 'f3');
-chess_gm.make_move('B', 'P', 'd7', 'd5');
+// chess_gm.make_move('W', 'N', 'g1', 'f3');
+// chess_gm.make_move('B', 'P', 'd7', 'd5');
 
-chess_gm.make_move('W', 'P', 'e4', 'd5');
-chess_gm.make_move('B', 'Q', 'd8', 'd5');
+// chess_gm.make_move('W', 'P', 'e4', 'd5');
+// chess_gm.make_move('B', 'Q', 'd8', 'd5');
 
 
-console.log(chess_gm);
-console.log(chess_gm.game.moves);
-console.log(chess_gm.game.board_position.board_position);
+// console.log(chess_gm);
+// console.log(chess_gm.game.moves);
+// console.log(chess_gm.game.board_position.board_position);
 
 ReactDOM.render(<GameLoader />, document.getElementById('root'));
